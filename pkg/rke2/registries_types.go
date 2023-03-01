082d57da798ab00702f97890381515dbf4e2ff94
Bas van Kampen <bas.vankampen@suse.com>
2023-03-01 17:10:37 +0100

    add json tags to registry types
    Signed-off-by: Bas van Kampen <bas.vankampen@suse.com>

--- pkg/rke2/registries_types.go
+++ pkg/rke2/registries_types.go
@@ -29,52 +29,52 @@ type Mirror struct {
 	// one by one until a working one is found. The endpoint must be a valid url
 	// with host specified.
 	// The scheme, host and path from the endpoint URL will be used.
-	Endpoint []string `toml:"endpoint" yaml:"endpoint"`
+	Endpoint []string `toml:"endpoint" yaml:"endpoint" json:"endpoint"`
 
 	// Rewrites are repository rewrite rules for a namespace. When fetching image resources
 	// from an endpoint and a key matches the repository via regular expression matching
 	// it will be replaced with the corresponding value from the map in the resource request.
-	Rewrite map[string]string `toml:"rewrite" yaml:"rewrite,omitempty"`
+	Rewrite map[string]string `toml:"rewrite" yaml:"rewrite,omitempty" json:"rewrite,omitempty"`
 }
 
 // AuthConfig contains the config related to authentication to a specific registry
 type AuthConfig struct {
 	// Username is the username to login the registry.
-	Username string `toml:"username" yaml:"username,omitempty"`
+	Username string `toml:"username" yaml:"username,omitempty" json:"username,omitempty"`
 	// Password is the password to login the registry.
-	Password string `toml:"password" yaml:"password,omitempty"`
+	Password string `toml:"password" yaml:"password,omitempty" json:"password,omitempty"`
 	// Auth is a base64 encoded string from the concatenation of the username,
 	// a colon, and the password.
-	Auth string `toml:"auth" yaml:"auth,omitempty"`
+	Auth string `toml:"auth" yaml:"auth,omitempty" json:"auth,omitempty"`
 	// IdentityToken is used to authenticate the user and get
 	// an access token for the registry.
-	IdentityToken string `toml:"identitytoken" yaml:"identity_token,omitempty"`
+	IdentityToken string `toml:"identitytoken" yaml:"identity_token,omitempty" json:"identity_token,omitempty"`
 }
 
 // TLSConfig contains the CA/Cert/Key used for a registry
 type TLSConfig struct {
-	CAFile             string `toml:"ca_file" yaml:"ca_file"`
-	CertFile           string `toml:"cert_file" yaml:"cert_file"`
-	KeyFile            string `toml:"key_file" yaml:"key_file"`
-	InsecureSkipVerify bool   `toml:"insecure_skip_verify" yaml:"insecure_skip_verify"`
+	CAFile             string `toml:"ca_file" yaml:"ca_file" json:"ca_file"`
+	CertFile           string `toml:"cert_file" yaml:"cert_file" json:"cert_file"`
+	KeyFile            string `toml:"key_file" yaml:"key_file" json:"key_file"`
+	InsecureSkipVerify bool   `toml:"insecure_skip_verify" yaml:"insecure_skip_verify" json:"insecure_skip_verify"`
 }
 
 // Registry is registry settings including mirrors, TLS, and credentials
 type Registry struct {
 	// Mirrors are namespace to mirror mapping for all namespaces.
-	Mirrors map[string]Mirror `toml:"mirrors" yaml:"mirrors"`
+	Mirrors map[string]Mirror `toml:"mirrors" yaml:"mirrors" json:"mirrors"`
 	// Configs are configs for each registry.
 	// The key is the FDQN or IP of the registry.
-	Configs map[string]RegistryConfig `toml:"configs" yaml:"configs"`
+	Configs map[string]RegistryConfig `toml:"configs" yaml:"configs" json:"configs"`
 }
 
 // RegistryConfig contains configuration used to communicate with the registry.
 type RegistryConfig struct {
 	// Auth contains information to authenticate to the registry.
-	Auth *AuthConfig `toml:"auth" yaml:"auth,omitempty"`
+	Auth *AuthConfig `toml:"auth" yaml:"auth,omitempty" json:"auth,omitempty"`
 	// TLS is a pair of CA/Cert/Key which then are used when creating the transport
 	// that communicates with the registry.
-	TLS *TLSConfig `toml:"tls" yaml:"tls,omitempty"`
+	TLS *TLSConfig `toml:"tls" yaml:"tls,omitempty" json:"tls,omitempty"`
 }
 
 // RKE2ConfigRegistry is a wrapper around the Registry struct to provide
