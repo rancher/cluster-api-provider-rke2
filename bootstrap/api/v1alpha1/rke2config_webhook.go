5ae37bda04f667420280c9def652f8683511c12b
Alexandr Demicev <alexandr.demicev@suse.com>
2023-03-27 13:12:03 +0200

    Validate ignition config using webhook
    Signed-off-by: Alexandr Demicev <alexandr.demicev@suse.com>

--- bootstrap/api/v1alpha1/rke2config_webhook.go
+++ bootstrap/api/v1alpha1/rke2config_webhook.go
@@ -17,14 +17,17 @@ limitations under the License.
 package v1alpha1
 
 import (
+	"fmt"
+
+	clct "github.com/flatcar/container-linux-config-transpiler/config"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/validation/field"
 	ctrl "sigs.k8s.io/controller-runtime"
-	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 )
 
-// log is for logging in this package.
-var rke2configlog = logf.Log.WithName("rke2config-resource")
+var cannotUseWithIgnition = fmt.Sprintf("not supported when spec.format is set to %q", Ignition)
 
 // SetupWebhookWithManager sets up and registers the webhook with the manager.
 func (r *RKE2Config) SetupWebhookWithManager(mgr ctrl.Manager) error {
@@ -39,7 +42,13 @@ var _ webhook.Defaulter = &RKE2Config{}
 
 // Default implements webhook.Defaulter so a webhook will be registered for the type.
 func (r *RKE2Config) Default() {
-	rke2configlog.Info("default", "name", r.Name)
+	defaultRKE2ConfigSpec(&r.Spec)
+}
+
+func defaultRKE2ConfigSpec(spec *RKE2ConfigSpec) {
+	if spec.AgentConfig.Format == "" {
+		spec.AgentConfig.Format = CloudConfig
+	}
 }
 
 //+kubebuilder:webhook:path=/validate-bootstrap-cluster-x-k8s-io-v1alpha1-rke2config,mutating=false,failurePolicy=fail,sideEffects=None,groups=bootstrap.cluster.x-k8s.io,resources=rke2configs,verbs=create;update,versions=v1alpha1,name=vrke2config.kb.io,admissionReviewVersions=v1
@@ -48,21 +57,65 @@ var _ webhook.Validator = &RKE2Config{}
 
 // ValidateCreate implements webhook.Validator so a webhook will be registered for the type.
 func (r *RKE2Config) ValidateCreate() error {
-	rke2configlog.Info("validate create", "name", r.Name)
-
-	return nil
+	return validateRKE2ConfigSpec(r.Name, &r.Spec)
 }
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type.
-func (r *RKE2Config) ValidateUpdate(old runtime.Object) error {
-	rke2configlog.Info("validate update", "name", r.Name)
-
-	return nil
+func (r *RKE2Config) ValidateUpdate(_ runtime.Object) error {
+	return validateRKE2ConfigSpec(r.Name, &r.Spec)
 }
 
 // ValidateDelete implements webhook.Validator so a webhook will be registered for the type.
 func (r *RKE2Config) ValidateDelete() error {
-	rke2configlog.Info("validate delete", "name", r.Name)
-
 	return nil
+}
+
+func validateRKE2ConfigSpec(name string, spec *RKE2ConfigSpec) error {
+	allErrs := spec.validate(field.NewPath("spec"))
+
+	if len(allErrs) == 0 {
+		return nil
+	}
+
+	return apierrors.NewInvalid(GroupVersion.WithKind("RKE2Config").GroupKind(), name, allErrs)
+}
+
+func (s *RKE2ConfigSpec) validate(pathPrefix *field.Path) field.ErrorList {
+	var allErrs field.ErrorList
+
+	allErrs = append(allErrs, s.validateIgnition(pathPrefix)...)
+
+	return allErrs
+}
+
+func (s *RKE2ConfigSpec) validateIgnition(pathPrefix *field.Path) field.ErrorList {
+	var allErrs field.ErrorList
+
+	if s.AgentConfig.Format == Ignition {
+		_, _, reports := clct.Parse([]byte(s.AgentConfig.AdditionalUserData.Config))
+		if (len(reports.Entries) > 0 && s.AgentConfig.AdditionalUserData.Strict) || reports.IsFatal() {
+			allErrs = append(
+				allErrs,
+				field.Invalid(
+					pathPrefix.Child("agentConfig.AdditionalUserData.Config"),
+					s.AgentConfig.AdditionalUserData.Config,
+					fmt.Sprintf("error parsing Container Linux Config: %v", reports.String()),
+				),
+			)
+		}
+	}
+
+	for i, file := range s.Files {
+		if file.Encoding == Gzip || file.Encoding == GzipBase64 {
+			allErrs = append(
+				allErrs,
+				field.Forbidden(
+					pathPrefix.Child("files").Index(i).Child("encoding"),
+					cannotUseWithIgnition,
+				),
+			)
+		}
+	}
+
+	return allErrs
 }
