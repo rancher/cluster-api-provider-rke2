f33991396e97460c5c21d168b1666f11f0773953
Alexandr Demicev <alexandr.demicev@suse.com>
2024-03-27 11:26:28 +0100

    Build OpenSUSE image for CAPRKE2
    Signed-off-by: Alexandr Demicev <alexandr.demicev@suse.com>

---
+++ image-builder/files/features.py
@@ -0,0 +1,90 @@
+# This file is part of cloud-init. See LICENSE file for license information.
+"""
+Feature flags are used as a way to easily toggle configuration
+**at build time**. They are provided to accommodate feature deprecation and
+downstream configuration changes.
+
+Currently used upstream values for feature flags are set in
+``cloudinit/features.py``. Overrides to these values should be
+patched directly (e.g., via quilt patch) by downstreams.
+
+Each flag should include a short comment regarding the reason for
+the flag and intended lifetime.
+
+Tests are required for new feature flags, and tests must verify
+all valid states of a flag, not just the default state.
+"""
+import re
+import sys
+from typing import Dict
+
+ERROR_ON_USER_DATA_FAILURE = False
+"""
+If there is a failure in obtaining user data (i.e., #include or
+decompress fails) and ``ERROR_ON_USER_DATA_FAILURE`` is ``False``,
+cloud-init will log a warning and proceed.  If it is ``True``,
+cloud-init will instead raise an exception.
+
+As of 20.3, ``ERROR_ON_USER_DATA_FAILURE`` is ``True``.
+
+(This flag can be removed after Focal is no longer supported.)
+"""
+
+
+ALLOW_EC2_MIRRORS_ON_NON_AWS_INSTANCE_TYPES = False
+"""
+When configuring apt mirrors, if
+``ALLOW_EC2_MIRRORS_ON_NON_AWS_INSTANCE_TYPES`` is ``True`` cloud-init
+will detect that a datasource's ``availability_zone`` property looks
+like an EC2 availability zone and set the ``ec2_region`` variable when
+generating mirror URLs; this can lead to incorrect mirrors being
+configured in clouds whose AZs follow EC2's naming pattern.
+
+As of 20.3, ``ALLOW_EC2_MIRRORS_ON_NON_AWS_INSTANCE_TYPES`` is ``False``
+so we no longer include ``ec2_region`` in mirror determination on
+non-AWS cloud platforms.
+
+If the old behavior is desired, users can provide the appropriate
+mirrors via :py:mod:`apt: <cloudinit.config.cc_apt_configure>`
+directives in cloud-config.
+"""
+
+
+EXPIRE_APPLIES_TO_HASHED_USERS = True
+"""
+If ``EXPIRE_APPLIES_TO_HASHED_USERS`` is True, then when expire is set true
+in cc_set_passwords, hashed passwords will be expired. Previous to 22.3,
+only non-hashed passwords were expired.
+
+(This flag can be removed after Jammy is no longer supported.)
+"""
+
+NETPLAN_CONFIG_ROOT_READ_ONLY = True
+"""
+If ``NETPLAN_CONFIG_ROOT_READ_ONLY`` is True, then netplan configuration will
+be written as a single root readon-only file /etc/netplan/50-cloud-init.yaml.
+This prevents wifi passwords in network v2 configuration from being
+world-readable. Prior to 23.1, netplan configuration is world-readable.
+
+(This flag can be removed after Jammy is no longer supported.)
+"""
+
+
+NOCLOUD_SEED_URL_APPEND_FORWARD_SLASH = True
+"""
+Append a forward slash '/' if NoCloud seedurl does not end with either
+a querystring or forward slash. Prior to 23.1, nocloud seedurl would be used
+unaltered, appending meta-data, user-data and vendor-data to without URL path
+separators.
+
+(This flag can be removed when Jammy is no longer supported.)
+"""
+
+
+def get_features() -> Dict[str, bool]:
+    """Return a dict of applicable features/overrides and their values."""
+    return {
+        k: getattr(sys.modules["cloudinit.features"], k)
+        for k in sys.modules["cloudinit.features"].__dict__.keys()
+        if re.match(r"^[_A-Z0-9]+$", k)
+    }
--- image-builder/scripts/opensuse/bootstrap.sh
+++ image-builder/scripts/opensuse/bootstrap.sh
@@ -2,20 +2,72 @@
 set -o errexit
 set -o nounset
 set -o pipefail
+set -o xtrace
+
+setup_infrastructure () {
+  if [[ "$1" == "aws" ]]; then
+    zypper --gpg-auto-import-keys --non-interactive install awscli amazon-ssm-agent
+    systemctl enable amazon-ssm-agent
+  fi
+}
+
+configure_systemd () {
+  echo "Enabling required systemd services"
+
+  systemctl enable sshd
+  systemctl enable cloud-final
+  systemctl enable cloud-config
+  systemctl enable cloud-init
+  systemctl enable cloud-init-local
+  
+  systemctl stop cloud-final
+  systemctl stop cloud-config
+  systemctl stop cloud-init
+  systemctl stop cloud-init-local
+}
+
+configure_cloudinit () {
+  echo "Configuring cloud-init"
+
+  cloud-init clean -s -l
+  rm -f /var/log/cloud-init*
+  rm -rf /var/lib/cloud/*
+
+  mkdir -p /usr/lib/python3/dist-packages/cloudinit
+  mv /tmp/features.py /usr/lib/python3.6/site-packages/cloudinit/features.py
+}
+
+cleanup_ssh_keys () {
+  echo "Cleaning up SSH keys"
+
+  rm -rf /etc/ssh/ssh_host_*
+  rm -rf /root/.ssh/authorized_keys
+  rm -rf /home/ec2-user/.ssh/authorized_keys
+}
+
+echo "Provisioning instance for $2"
 
 echo "Install required packages"
+
 zypper --gpg-auto-import-keys ref && \
 zypper --gpg-auto-import-keys --non-interactive install \
         curl \
         openssh-server \
         cloud-init \
         systemd \
+        openssh \
 
 echo "Install RKE2 components"
+
 mkdir -p /opt/rke2-artifacts
-curl -sfL -o /opt/rke2-artifacts/rke2-images.linux-amd64.tar.zst https://github.com/rancher/rke2/releases/download/v${3}/rke2-images.linux-amd64.tar.zst
-curl -sfL -o /opt/rke2-artifacts/rke2.linux-amd64.tar.gz https://github.com/rancher/rke2/releases/download/v${3}/rke2.linux-amd64.tar.gz
-curl -sfL -o /opt/rke2-artifacts/sha256sum-amd64.txt https://github.com/rancher/rke2/releases/download/v${3}/sha256sum-amd64.txt
+curl -sfL -o /opt/rke2-artifacts/rke2-images.linux-amd64.tar.zst https://github.com/rancher/rke2/releases/download/v${1}/rke2-images.linux-amd64.tar.zst
+curl -sfL -o /opt/rke2-artifacts/rke2.linux-amd64.tar.gz https://github.com/rancher/rke2/releases/download/v${1}/rke2.linux-amd64.tar.gz
+curl -sfL -o /opt/rke2-artifacts/sha256sum-amd64.txt https://github.com/rancher/rke2/releases/download/v${1}/sha256sum-amd64.txt
 curl -sfL -o /opt/install.sh https://get.rke2.io
+
+configure_systemd 
+configure_cloudinit
+cleanup_ssh_keys
+setup_infrastructure $2
 
 echo "Done"
