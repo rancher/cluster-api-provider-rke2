cd6644bb1f8312055668a515025ac04ae78e66f2
Carlos Salas <carlos.salas@suse.com>
2024-08-29 16:37:50 +0200

    test: add clusterclass scenario to e2e
    Signed-off-by: Carlos Salas <carlos.salas@suse.com>

--- test/e2e/helpers.go
+++ test/e2e/helpers.go
@@ -29,6 +29,7 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/pkg/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/klog/v2"
 
@@ -51,19 +52,41 @@ const (
 
 // ApplyClusterTemplateAndWaitInput is the input type for ApplyClusterTemplateAndWait.
 type ApplyClusterTemplateAndWaitInput struct {
-	ClusterProxy                   framework.ClusterProxy
-	ConfigCluster                  clusterctl.ConfigClusterInput
-	WaitForClusterIntervals        []interface{}
-	WaitForControlPlaneIntervals   []interface{}
-	WaitForMachineDeployments      []interface{}
-	Args                           []string // extra args to be used during `kubectl apply`
-	PreWaitForCluster              func()
-	PostMachinesProvisioned        func()
-	WaitForControlPlaneInitialized Waiter
+	ClusterProxy                 framework.ClusterProxy
+	ConfigCluster                clusterctl.ConfigClusterInput
+	WaitForClusterIntervals      []interface{}
+	WaitForControlPlaneIntervals []interface{}
+	WaitForMachineDeployments    []interface{}
+	Args                         []string // extra args to be used during `kubectl apply`
+	PreWaitForCluster            func()
+	PostMachinesProvisioned      func()
+	ControlPlaneWaiters
+}
+
+// ApplyCustomClusterTemplateAndWaitInput is the input type for ApplyCustomClusterTemplateAndWait.
+type ApplyCustomClusterTemplateAndWaitInput struct {
+	ClusterProxy                 framework.ClusterProxy
+	CustomTemplateYAML           []byte
+	ClusterName                  string
+	Namespace                    string
+	Flavor                       string
+	WaitForClusterIntervals      []interface{}
+	WaitForControlPlaneIntervals []interface{}
+	WaitForMachineDeployments    []interface{}
+	Args                         []string // extra args to be used during `kubectl apply`
+	PreWaitForCluster            func()
+	PostMachinesProvisioned      func()
+	ControlPlaneWaiters
 }
 
 // Waiter is a function that runs and waits for a long-running operation to finish and updates the result.
-type Waiter func(ctx context.Context, input ApplyClusterTemplateAndWaitInput, result *ApplyClusterTemplateAndWaitResult)
+type Waiter func(ctx context.Context, input ApplyCustomClusterTemplateAndWaitInput, result *ApplyCustomClusterTemplateAndWaitResult)
+
+// ControlPlaneWaiters are Waiter functions for the control plane.
+type ControlPlaneWaiters struct {
+	WaitForControlPlaneInitialized   Waiter
+	WaitForControlPlaneMachinesReady Waiter
+}
 
 // ApplyClusterTemplateAndWaitResult is the output type for ApplyClusterTemplateAndWait.
 type ApplyClusterTemplateAndWaitResult struct {
@@ -73,10 +96,17 @@ type ApplyClusterTemplateAndWaitResult struct {
 	MachineDeployments []*clusterv1.MachineDeployment
 }
 
+// ApplyCustomClusterTemplateAndWaitResult is the output type for ApplyCustomClusterTemplateAndWait.
+type ApplyCustomClusterTemplateAndWaitResult struct {
+	ClusterClass       *clusterv1.ClusterClass
+	Cluster            *clusterv1.Cluster
+	ControlPlane       *controlplanev1.RKE2ControlPlane
+	MachineDeployments []*clusterv1.MachineDeployment
+}
+
 // ApplyClusterTemplateAndWait gets a managed cluster template using clusterctl, and waits for the cluster to be ready.
 // Important! this method assumes the cluster uses a RKE2ControlPlane and MachineDeployment.
 func ApplyClusterTemplateAndWait(ctx context.Context, input ApplyClusterTemplateAndWaitInput, result *ApplyClusterTemplateAndWaitResult) {
-	setDefaults(&input)
 	Expect(ctx).NotTo(BeNil(), "ctx is required for ApplyClusterTemplateAndWait")
 	Expect(input.ClusterProxy).ToNot(BeNil(), "Invalid argument. input.ClusterProxy can't be nil when calling ApplyManagedClusterTemplateAndWait")
 	Expect(result).ToNot(BeNil(), "Invalid argument. result can't be nil when calling ApplyClusterTemplateAndWait")
@@ -108,37 +138,93 @@ func ApplyClusterTemplateAndWait(ctx context.Context, input ApplyClusterTemplate
 	})
 	Expect(workloadClusterTemplate).ToNot(BeNil(), "Failed to get the cluster template")
 
-	By("Applying the cluster template yaml to the cluster")
+	// Ensure we have a Cluster for dump and cleanup steps in AfterEach even if ApplyClusterTemplateAndWait fails.
+	result.Cluster = &clusterv1.Cluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      input.ConfigCluster.ClusterName,
+			Namespace: input.ConfigCluster.Namespace,
+		},
+	}
+
+	ApplyCustomClusterTemplateAndWait(ctx, ApplyCustomClusterTemplateAndWaitInput{
+		ClusterProxy:                 input.ClusterProxy,
+		CustomTemplateYAML:           workloadClusterTemplate,
+		ClusterName:                  input.ConfigCluster.ClusterName,
+		Namespace:                    input.ConfigCluster.Namespace,
+		Flavor:                       input.ConfigCluster.Flavor,
+		WaitForClusterIntervals:      input.WaitForClusterIntervals,
+		WaitForControlPlaneIntervals: input.WaitForControlPlaneIntervals,
+		WaitForMachineDeployments:    input.WaitForMachineDeployments,
+		PreWaitForCluster:            input.PreWaitForCluster,
+		PostMachinesProvisioned:      input.PostMachinesProvisioned,
+		ControlPlaneWaiters:          input.ControlPlaneWaiters,
+	}, (*ApplyCustomClusterTemplateAndWaitResult)(result))
+}
+
+// ApplyCustomClusterTemplateAndWait deploys a cluster from a custom yaml file, and waits for the cluster to be ready.
+// Important! this method assumes the cluster uses a RKE2ControlPlane and MachineDeployment.
+func ApplyCustomClusterTemplateAndWait(ctx context.Context, input ApplyCustomClusterTemplateAndWaitInput, result *ApplyCustomClusterTemplateAndWaitResult) {
+	setDefaults(&input)
+	Expect(ctx).NotTo(BeNil(), "ctx is required for ApplyCustomClusterTemplateAndWait")
+	Expect(input.ClusterProxy).ToNot(BeNil(), "Invalid argument. input.ClusterProxy can't be nil when calling ApplyCustomClusterTemplateAndWait")
+	Expect(input.CustomTemplateYAML).NotTo(BeEmpty(), "Invalid argument. input.CustomTemplateYAML can't be empty when calling ApplyCustomClusterTemplateAndWait")
+	Expect(input.ClusterName).NotTo(BeEmpty(), "Invalid argument. input.ClusterName can't be empty when calling ApplyCustomClusterTemplateAndWait")
+	Expect(input.Namespace).NotTo(BeEmpty(), "Invalid argument. input.Namespace can't be empty when calling ApplyCustomClusterTemplateAndWait")
+	Expect(result).ToNot(BeNil(), "Invalid argument. result can't be nil when calling ApplyClusterTemplateAndWait")
+
+	Byf("Creating the workload cluster with name %q from the provided yaml", input.ClusterName)
+
+	// Ensure we have a Cluster for dump and cleanup steps in AfterEach even if ApplyClusterTemplateAndWait fails.
+	result.Cluster = &clusterv1.Cluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      input.ClusterName,
+			Namespace: input.Namespace,
+		},
+	}
+
+	Byf("Applying the cluster template yaml of cluster %s", klog.KRef(input.Namespace, input.ClusterName))
 	Eventually(func() error {
-		return input.ClusterProxy.Apply(ctx, workloadClusterTemplate, input.Args...)
-	}, 10*time.Second).Should(Succeed(), "Failed to apply the cluster template")
+		return input.ClusterProxy.Apply(ctx, input.CustomTemplateYAML, input.Args...)
+		// return input.ClusterProxy.CreateOrUpdate(ctx, input.CustomTemplateYAML, input.CreateOrUpdateOpts...)
+	}, 1*time.Minute).Should(Succeed(), "Failed to apply the cluster template")
 
 	// Once we applied the cluster template we can run PreWaitForCluster.
 	// Note: This can e.g. be used to verify the BeforeClusterCreate lifecycle hook is executed
 	// and blocking correctly.
 	if input.PreWaitForCluster != nil {
-		By("Calling PreWaitForCluster")
+		Byf("Calling PreWaitForCluster for cluster %s", klog.KRef(input.Namespace, input.ClusterName))
 		input.PreWaitForCluster()
 	}
 
-	By("Waiting for the cluster infrastructure to be provisioned")
+	Byf("Waiting for the cluster infrastructure of cluster %s to be provisioned", klog.KRef(input.Namespace, input.ClusterName))
 	result.Cluster = framework.DiscoveryAndWaitForCluster(ctx, framework.DiscoveryAndWaitForClusterInput{
 		Getter:    input.ClusterProxy.GetClient(),
-		Namespace: input.ConfigCluster.Namespace,
-		Name:      input.ConfigCluster.ClusterName,
+		Namespace: input.Namespace,
+		Name:      input.ClusterName,
 	}, input.WaitForClusterIntervals...)
 
-	By("Waiting for RKE2 control plane to be initialized")
+	if result.Cluster.Spec.Topology != nil {
+		result.ClusterClass = framework.GetClusterClassByName(ctx, framework.GetClusterClassByNameInput{
+			Getter:    input.ClusterProxy.GetClient(),
+			Namespace: input.Namespace,
+			Name:      result.Cluster.Spec.Topology.Class,
+		})
+	}
+
+	Byf("Waiting for control plane of cluster %s to be initialized", klog.KRef(input.Namespace, input.ClusterName))
 	input.WaitForControlPlaneInitialized(ctx, input, result)
 
-	Byf("Waiting for the machine deployments to be provisioned")
-	result.MachineDeployments = DiscoveryAndWaitForMachineDeployments(ctx, framework.DiscoveryAndWaitForMachineDeploymentsInput{
+	Byf("Waiting for control plane of cluster %s to be ready", klog.KRef(input.Namespace, input.ClusterName))
+	input.WaitForControlPlaneMachinesReady(ctx, input, result)
+
+	Byf("Waiting for the machine deployments of cluster %s to be provisioned", klog.KRef(input.Namespace, input.ClusterName))
+	result.MachineDeployments = framework.DiscoveryAndWaitForMachineDeployments(ctx, framework.DiscoveryAndWaitForMachineDeploymentsInput{
 		Lister:  input.ClusterProxy.GetClient(),
 		Cluster: result.Cluster,
 	}, input.WaitForMachineDeployments...)
 
 	if input.PostMachinesProvisioned != nil {
-		By("Calling PostMachinesProvisioned")
+		Byf("Calling PostMachinesProvisioned for cluster %s", klog.KRef(input.Namespace, input.ClusterName))
 		input.PostMachinesProvisioned()
 	}
 }
@@ -229,6 +315,75 @@ func GetRKE2ControlPlaneByCluster(ctx context.Context, input GetRKE2ControlPlane
 		return &controlPlaneList.Items[0]
 	}
 	return nil
+}
+
+// WaitForControlPlaneAndMachinesReadyInput is the input type for WaitForControlPlaneAndMachinesReady.
+type WaitForControlPlaneAndMachinesReadyInput struct {
+	GetLister    framework.GetLister
+	Cluster      *clusterv1.Cluster
+	ControlPlane *controlplanev1.RKE2ControlPlane
+}
+
+// WaitForControlPlaneAndMachinesReady waits for a RKE2ControlPlane object to be ready (all the machine provisioned and one node ready).
+func WaitForControlPlaneAndMachinesReady(ctx context.Context, input WaitForControlPlaneAndMachinesReadyInput, intervals ...interface{}) {
+	Expect(ctx).NotTo(BeNil(), "ctx is required for WaitForControlPlaneReady")
+	Expect(input.GetLister).ToNot(BeNil(), "Invalid argument. input.GetLister can't be nil when calling WaitForControlPlaneReady")
+	Expect(input.Cluster).ToNot(BeNil(), "Invalid argument. input.Cluster can't be nil when calling WaitForControlPlaneReady")
+	Expect(input.ControlPlane).ToNot(BeNil(), "Invalid argument. input.ControlPlane can't be nil when calling WaitForControlPlaneReady")
+
+	if input.ControlPlane.Spec.Replicas != nil && int(*input.ControlPlane.Spec.Replicas) > 1 {
+		Byf("Waiting for the remaining control plane machines managed by %s to be provisioned", klog.KObj(input.ControlPlane))
+		WaitForRKE2ControlPlaneMachinesToExist(ctx, WaitForRKE2ControlPlaneMachinesToExistInput{
+			Lister:       input.GetLister,
+			Cluster:      input.Cluster,
+			ControlPlane: input.ControlPlane,
+		}, intervals...)
+	}
+
+	Byf("Waiting for control plane %s to be ready (implies underlying nodes to be ready as well)", klog.KObj(input.ControlPlane))
+	waitForControlPlaneToBeReadyInput := WaitForControlPlaneToBeReadyInput{
+		Getter:       input.GetLister,
+		ControlPlane: client.ObjectKeyFromObject(input.ControlPlane),
+	}
+	WaitForControlPlaneToBeReady(ctx, waitForControlPlaneToBeReadyInput, intervals...)
+
+	framework.AssertControlPlaneFailureDomains(ctx, framework.AssertControlPlaneFailureDomainsInput{
+		Lister:  input.GetLister,
+		Cluster: input.Cluster,
+	})
+}
+
+// WaitForRKE2ControlPlaneMachinesToExistInput is the input for WaitForRKE2ControlPlaneMachinesToExist.
+type WaitForRKE2ControlPlaneMachinesToExistInput struct {
+	Lister       framework.Lister
+	Cluster      *clusterv1.Cluster
+	ControlPlane *controlplanev1.RKE2ControlPlane
+}
+
+// WaitForRKE2ControlPlaneMachinesToExist will wait until all control plane machines have node refs.
+func WaitForRKE2ControlPlaneMachinesToExist(ctx context.Context, input WaitForRKE2ControlPlaneMachinesToExistInput, intervals ...interface{}) {
+	By("Waiting for all control plane nodes to exist")
+	inClustersNamespaceListOption := client.InNamespace(input.Cluster.Namespace)
+	// ControlPlane labels
+	matchClusterListOption := client.MatchingLabels{
+		clusterv1.MachineControlPlaneLabel: "",
+		clusterv1.ClusterNameLabel:         input.Cluster.Name,
+	}
+
+	Eventually(func() (int, error) {
+		machineList := &clusterv1.MachineList{}
+		if err := input.Lister.List(ctx, machineList, inClustersNamespaceListOption, matchClusterListOption); err != nil {
+			Byf("Failed to list the machines: %+v", err)
+			return 0, err
+		}
+		count := 0
+		for _, machine := range machineList.Items {
+			if machine.Status.NodeRef != nil {
+				count++
+			}
+		}
+		return count, nil
+	}, intervals...).Should(Equal(int(*input.ControlPlane.Spec.Replicas)), "Timed out waiting for %d control plane machines to exist", int(*input.ControlPlane.Spec.Replicas))
 }
 
 // WaitForControlPlaneToBeReadyInput is the input for WaitForControlPlaneToBeReady.
@@ -328,12 +483,24 @@ func WaitForClusterToUpgrade(ctx context.Context, input WaitForClusterToUpgradeI
 	}, intervals...).Should(BeTrue(), framework.PrettyPrint(input.ControlPlane))
 }
 
-func setDefaults(input *ApplyClusterTemplateAndWaitInput) {
+// setDefaults sets the default values for ApplyCustomClusterTemplateAndWaitInput if not set.
+// Currently, we set the default ControlPlaneWaiters here, which are implemented for RKE2ControlPlane.
+func setDefaults(input *ApplyCustomClusterTemplateAndWaitInput) {
 	if input.WaitForControlPlaneInitialized == nil {
-		input.WaitForControlPlaneInitialized = func(ctx context.Context, input ApplyClusterTemplateAndWaitInput, result *ApplyClusterTemplateAndWaitResult) {
+		input.WaitForControlPlaneInitialized = func(ctx context.Context, input ApplyCustomClusterTemplateAndWaitInput, result *ApplyCustomClusterTemplateAndWaitResult) {
 			result.ControlPlane = DiscoveryAndWaitForRKE2ControlPlaneInitialized(ctx, DiscoveryAndWaitForRKE2ControlPlaneInitializedInput{
 				Lister:  input.ClusterProxy.GetClient(),
 				Cluster: result.Cluster,
+			}, input.WaitForControlPlaneIntervals...)
+		}
+	}
+
+	if input.WaitForControlPlaneMachinesReady == nil {
+		input.WaitForControlPlaneMachinesReady = func(ctx context.Context, input ApplyCustomClusterTemplateAndWaitInput, result *ApplyCustomClusterTemplateAndWaitResult) {
+			WaitForControlPlaneAndMachinesReady(ctx, WaitForControlPlaneAndMachinesReadyInput{
+				GetLister:    input.ClusterProxy.GetClient(),
+				Cluster:      result.Cluster,
+				ControlPlane: result.ControlPlane,
 			}, input.WaitForControlPlaneIntervals...)
 		}
 	}
