3325dd0b8221229d96cc837c0b8e1c7fbce28842
Tim Treptow <ttreptow@domaintools.com>
2024-12-13 05:53:04 -0800

    Rotate kubeconfig before it expires

--- controlplane/internal/controllers/rke2controlplane_controller.go
+++ controlplane/internal/controllers/rke2controlplane_controller.go
@@ -19,6 +19,7 @@ package controllers
 import (
 	"context"
 	"fmt"
+	"sigs.k8s.io/cluster-api/util/certs"
 	"strings"
 	"time"
 
@@ -58,6 +59,7 @@ import (
 	"github.com/rancher/cluster-api-provider-rke2/pkg/registration"
 	"github.com/rancher/cluster-api-provider-rke2/pkg/rke2"
 	"github.com/rancher/cluster-api-provider-rke2/pkg/secret"
+	capikubeconfig "sigs.k8s.io/cluster-api/util/kubeconfig"
 )
 
 const (
@@ -815,6 +817,19 @@ func (r *RKE2ControlPlaneReconciler) reconcileKubeconfig(
 	// only do rotation on owned secrets
 	if !util.IsControlledBy(configSecret, rcp) {
 		return ctrl.Result{}, nil
+	}
+
+	needsRotation, err := capikubeconfig.NeedsClientCertRotation(configSecret, certs.ClientCertificateRenewalDuration)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	if needsRotation {
+		logger.Info("Rotating kubeconfig secret")
+		err = kubeconfig.CreateSecretWithOwner(ctx, r.Client, clusterName, endpoint.String(), controllerOwnerRef)
+		if err != nil {
+			return ctrl.Result{}, errors.Wrap(err, "failed to regenerate kubeconfig")
+		}
 	}
 
 	return ctrl.Result{}, nil
